c474383ed6481d198c0ba41365210e96c59203af
Irish Manalang <irish.r.manalang.mil@army.mil>
2024-11-12 08:26:32 -0600

    initial backend with docker and app configuration complete

---
+++ src/main/java/com/portfolio/backend/BackendPortfolioWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPortfolioWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPortfolioWebsiteApplication.class, args);
+	}
+
+}
