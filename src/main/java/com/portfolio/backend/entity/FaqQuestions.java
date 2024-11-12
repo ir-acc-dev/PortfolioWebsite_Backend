554aa6a00a27a13e442cff8cb3360b529d992a53
Irish Manalang <irish.r.manalang.mil@army.mil>
2024-11-12 08:30:57 -0600

    set up entity and database

---
+++ src/main/java/com/portfolio/backend/entity/FaqQuestions.java
@@ -0,0 +1,20 @@
+package com.portfolio.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "FaqQuestions")
+public class FaqQuestions {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String question;
+}
